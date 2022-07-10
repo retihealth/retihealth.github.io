76538ec63a586490fe49c62bca6a1bc91f518712
George Prichard <dgmprichard@gmail.com>
2022-07-10 10:40:18 +0100

    About fixes

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,11 +5,11 @@
         <h1 class="text-5xl font-bold">About Reti Health</h1>
         <p class="text-xl py-6">
           We provide automatic hypertension screening using artificial
-          intelligence during routine eye exams Provident
+          intelligence during routine eye exams
         </p>
         <p class="py-6">
           Reti Health is a venture backed start-up transforming hypertension
-          screening.
+          screening
         </p>
       </div>
     </div>
